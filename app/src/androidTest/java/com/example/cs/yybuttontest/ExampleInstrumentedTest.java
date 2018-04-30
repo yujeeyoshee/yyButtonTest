5bd28be7b17af30511b9486c70eac3476d17a4a7
Yuji Yoshiie <yuji.yoshiie@crowdstrike.com>
2018-04-30 15:06:28 -0700

    Inititial commit for yyButtonTest

---
+++ app/src/androidTest/java/com/example/cs/yybuttontest/ExampleInstrumentedTest.java
@@ -0,0 +1,44 @@
+package com.example.cs.yybuttontest;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.cs.buttontest", appContext.getPackageName());
+    }
+
+    @Test
+    public void clickButtonTest() throws Exception {
+        Espresso.onView(withId(R.id.textView)).check(matches(withText("Hello World!")));
+        Espresso.onView(withId(R.id.button)).perform(click());
+        Espresso.onView(withId(R.id.textView)).check(matches(withText("Button clicked 1 time")));
+        Espresso.onView(withId(R.id.button)).perform(click());
+        Espresso.onView(withId(R.id.textView)).check(matches(withText("Button clicked 2 times")));
+    }
+}
---
+++ app/src/main/java/com/example/cs/yybuttontest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cs.yybuttontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                String timeTxt = count == 1 ? "time" : "times";
+                textView.setText("Button clicked " + count + " " + timeTxt);
+            }
+        });
+    }
+}
